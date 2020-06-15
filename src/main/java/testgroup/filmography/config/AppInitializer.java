a1ce3242409c1627c3d67aa4a9a34528a6ecdf5e
Oleksandr Leleko <o.leleko@smiddle.lab>
2020-06-15 21:47:11 +0300

    Second commit

---
+++ src/main/java/testgroup/filmography/config/AppInitializer.java
@@ -0,0 +1,25 @@
+package testgroup.filmography.config;
+
+//import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+       /* return new Class[0];*/
+        return new Class[]{HibernateConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+
+}
